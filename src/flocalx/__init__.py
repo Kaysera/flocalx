b5006218f8f3618439bf46546c63ba9f046910e2
Guillermo Fernandez <Guillermo.Fernandez@uclm.es>
2024-02-06 12:52:02 +0100

    Migrated to pyproject.toml

--- src/flocalx/__init__.py
+++ src/flocalx/__init__.py
@@ -19,7 +19,7 @@
 # "X.Y.dev0" is the canonical version of "X.Y.dev".
 
 # FLocalX package version
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 
 # =============================================================================
